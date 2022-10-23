8a5cde696925b92158c90f727ff65c2be8dc6208
bochinchero <bochinchero@bochinchero.bochinchero>
2022-10-23 15:53:51 +0100

    added logging of graph from ln-map

--- dcrln.py
+++ dcrln.py
@@ -2,8 +2,36 @@ import requests
 import pandas as pd
 from datetime import date
 import os
+import json
+from urllib.request import urlopen
+import numpy as np
 
 pd.set_option('display.max_columns', None)
+
+def convert_ln_json_to_df(jsonResponse):
+    graph_json = (jsonResponse)
+
+    df_nodes = pd.json_normalize(graph_json['nodes'])
+
+    df_channels = pd.json_normalize(graph_json['edges'])
+    df_channels.channel_id = df_channels.channel_id.astype(np.int64)
+    df_channels.capacity = df_channels.capacity.astype(int)
+
+    for index, row in df_nodes.iterrows():
+        ipk = row['pub_key']
+        node1 = df_channels[df_channels['node1_pub']==ipk]
+        outCount =  node1.channel_id.count().astype(int)
+        outCapacity = node1.capacity.sum().astype(int)
+        node2 = df_channels[df_channels['node2_pub']==ipk]
+        inCount =  node2.channel_id.count().astype(int)
+        inCapacity = node2.capacity.sum().astype(int)
+        df_nodes.loc[index, 'outCount'] = outCount
+        df_nodes.loc[index, 'outCapacity'] = outCapacity
+        df_nodes.loc[index, 'inCount'] = inCount
+        df_nodes.loc[index, 'inCapacity'] = inCapacity
+        df_nodes.loc[index, 'chCount'] = inCount+outCount
+
+    return df_nodes, df_channels
 
 url = 'https://ln-map.jholdstock.uk//api/graph'
 # get api response
@@ -20,3 +48,15 @@ filename = pathStr + todayStr + '.txt'
 # save raw json response to a csv
 with open(filename, "w", encoding="utf-8") as f:
     f.write(strResposne)
+
+response = urlopen(url)
+data_json = json.loads(response.read())
+df_nodes, df_channels = convert_ln_json_to_df(data_json)
+df_nodes = df_nodes.sort_values(by='chCount', ascending=False)
+
+pathStr = './data/lnd-nodes/' + yearMonthStr
+if not os.path.exists(pathStr):
+    os.makedirs(pathStr)
+filename = pathStr + todayStr + '.csv'
+# save to csv
+df_nodes.to_csv(filename)
