e92d7e9b6609439c536f61e092b1ebd2cf835e07
bochinchero <bochinchero@bochinchero.bochinchero>
2022-11-26 10:23:35 +0000

    update to fix broken ln stats

--- dcrln.py
+++ dcrln.py
@@ -33,21 +33,14 @@ def convert_ln_json_to_df(jsonResponse):
 
     return df_nodes, df_channels
 
-url = 'https://decred.lighting/api/graph'
-# get api response
-response = requests.get(url)
-strResposne = response.text
 # get today's date for file path
 todayStr = str(date.today())
 yearMonthStr = date.today().strftime("%Y/%m/")
-# path for data
-pathStr = './data/lnd-raw/' + yearMonthStr
-if not os.path.exists(pathStr):
-    os.makedirs(pathStr)
-filename = pathStr + todayStr + '.txt'
-# save raw json response to a csv
-with open(filename, "w", encoding="utf-8") as f:
-    f.write(strResposne)
+
+url = 'https://raw.githubusercontent.com/bochinchero/dcrsnapcsv/main/data/dcrlndgraph/' + yearMonthStr + todayStr +".json"
+# get api response
+response = requests.get(url)
+strResposne = response.text
 
 response = urlopen(url)
 data_json = json.loads(response.read())
