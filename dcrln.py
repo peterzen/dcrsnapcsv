a6698c49e6e529fad01f50195c126dc3f05a0961
bochinchero <bochinchero@bochinchero.bochinchero>
2022-12-22 13:58:40 +0000

    added logging of ln-map.jholdstock.uk

--- dcrln.py
+++ dcrln.py
@@ -34,9 +34,21 @@ def convert_ln_json_to_df(jsonResponse):
 
     return df_nodes, df_channels
 
+url = 'https://ln-map.jholdstock.uk/api/graph'
+# get api response
+response = requests.get(url)
+strResposne = response.text
 # get today's date for file path
 todayStr = str(date.today())
 yearMonthStr = date.today().strftime("%Y/%m/")
+# path for data
+pathStr = './data/lnd-raw/' + yearMonthStr
+if not os.path.exists(pathStr):
+    os.makedirs(pathStr)
+filename = pathStr + todayStr + '.txt'
+# save raw json response to a csv
+with open(filename, "w", encoding="utf-8") as f:
+    f.write(strResposne)
 
 url = 'https://raw.githubusercontent.com/bochinchero/dcrsnapcsv/main/data/dcrlndgraph/' + yearMonthStr + todayStr +".json"
 # get api response
